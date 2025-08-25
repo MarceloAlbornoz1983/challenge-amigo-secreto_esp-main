5885525e3be83a858cb7e7f538a6e2928955b730
MarceloAlbornoz1983 <m_albornozg@live.cl>
2025-08-24 22:28:42 -0400

    Se crea el array para almacenar los nombres y la función para validar y agregar amigos

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -1,1 +1,27 @@
+let amigos = [];
+
+
+function validarAmigo(){
+    let nombreAmigo = document.getElementById("amigo").value;
+    if(nombreAmigo == ""){
+        alert("Por favor, inserte un nombre");
+    }else{
+        amigos.push(nombreAmigo);
+    }
+    return;
+}
+
+function limpiarCaja(){
+    let valorCaja = document.getElementById("amigo");
+    valorCaja.value = "";
+}
+
+   
+
+function agregarAmigo(){
+    validarAmigo();  
+    console.log(amigos); 
+    limpiarCaja();
+
+}
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
