0da42a651084e7c74d358037650ed2d6803ecf60
MarceloAlbornoz1983 <m_albornozg@live.cl>
2025-08-24 23:46:48 -0400

    Se Implementa  función para actualizar la lista de amigos

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -1,4 +1,5 @@
 let amigos = [];
+let lista = document.getElementById("listaAmigos");
 
 
 function validarAmigo(){
@@ -16,12 +17,29 @@ function limpiarCaja(){
     valorCaja.value = "";
 }
 
-   
+function listarAmigos(){
+    
+    let elementosLista = "";
+    
+    for (let i = 0; i < amigos.length; i++) {
+        elementosLista += `<li>${amigos[i]}</li>`;
+    }
+    return lista.innerHTML = elementosLista;
+}
+
+function limpiarLista() {
+    lista.innerHTML = "";
+}
 
 function agregarAmigo(){
     validarAmigo();  
     console.log(amigos); 
     limpiarCaja();
+    limpiarLista();
+    listarAmigos();
 
 }
+
+
+
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
