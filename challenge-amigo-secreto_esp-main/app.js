c7911a270cf03a497b201c3c02405d36ef2068c8
MarceloAlbornoz1983 <m_albornozg@live.cl>
2025-08-25 22:59:01 -0400

    Se crea la funcion para sortear amigo

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -1,5 +1,6 @@
 let amigos = [];
 let lista = document.getElementById("listaAmigos");
+let resultadoSorteo = document.getElementById("resultado");
 
 
 function validarAmigo(){
@@ -40,6 +41,14 @@ function agregarAmigo(){
 
 }
 
-
+function sortearAmigo(){
+    if (amigos == "") {
+        alert("Debe ingresar amigos para el sorteo");
+    }else{
+        let numeroSorteado = Math.floor(Math.random() * amigos.length);
+        console.log(numeroSorteado);
+        resultadoSorteo.innerHTML = `<li>Te ha tocado como amigo secreto a: ${amigos[numeroSorteado]}`;
+    }
+}
 
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
